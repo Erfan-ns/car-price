ef27541b776ef9f3aed43bd0d1790103110f966f
Erfan nasiri <164219925+Erfan-ns@users.noreply.github.com>
2024-08-02 10:36:30 +0330

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-class Car:
+class CAR:
+    wheel = 4
+    obj_nums = 0
+
     def __init__(self, x, y):
         self.name = x
         self.price = y
-        print(f"{self.name} costs {self.price}")
+        print(f"{self.name} costs {self.price} and has got {CAR.wheel} wheels")
+        CAR.obj_nums += 1
 
 
-c1 = Car("benz", 900)
-c2 = Car("pride", 100)
+c1 = CAR("benz", 900)
+c2 = CAR("pride", 100)
+
+print(f"car count : {CAR.obj_nums}")
