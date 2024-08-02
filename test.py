b1e995bc48b9dec5664ce8a3b350be8e743f7134
Erfan nasiri <164219925+Erfan-ns@users.noreply.github.com>
2024-08-02 08:16:45 +0330

    Add files via upload

--- test.py
+++ test.py
@@ -1,15 +1,9 @@
-
 class Car:
     def __init__(self, x, y):
         self.name = x
         self.price = y
-
-    def show(self):
         print(f"{self.name} costs {self.price}")
 
 
 c1 = Car("benz", 900)
 c2 = Car("pride", 100)
-
-c1.show() 
-c2.show()
